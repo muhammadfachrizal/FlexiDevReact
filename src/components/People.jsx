3ebe5547a476e79b630efad28bbdb3a8bd34045b
FACHRIZAL-PC\FACHRIZAL <fachrizal.muhammad20@gmail.com>
2024-09-03 22:46:39 +0700

    Initial

---
+++ src/components/People.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+function People() {
+    const [selectedComponent, setSelectedComponent] = useState("People");
+    const [isLoading, setLoading] = useState(true);
+    const [starWarsDataPeople, setStarWarsDataPeople] = useState();
+    const [urlPeople, setUrlPeople] = useState(
+        `https://swapi.dev/api/people/?page=1`
+    );
+
+    useEffect(() => {
+        axios.get(urlPeople).then((response) => {
+            setStarWarsDataPeople(response.data);
+            setLoading(false);
+        });
+    }, [urlPeople]);
+
+    if (isLoading) {
+        return (
+            <div>
+                <div>
+                    <h1 className="txt-shadow-blue">People</h1>
+                    <button
+                        disabled={true}
+                    >
+                        ⏪ Previous Page
+                    </button>
+                    <button
+                        disabled={true}
+                    >
+                        Next Page⏩
+                    </button>
+                </div>
+                <div className="overlay">
+                    Loading...
+                </div>
+            </div>
+        );
+    }
+
+    const allPeopleOnPage = starWarsDataPeople.results.map((people) => {
+        console.log(people);
+
+        return (
+            <div className="card card-people" onClick={() => setSelectedComponent("Planets")}>
+                <h2 key={people.name}>{people.name}</h2>
+                <p>Gender: {people.gender}</p>
+                <p>Birth Year: {people.birth_year}</p>
+                <p>Height: {people.height}</p>
+                <p>Hair Color: {people.hair_color}</p>
+                <p>Skin Color: {people.skin_color}</p>
+                <p>Eye Color: {people.eye_color}</p>
+                <br />
+            </div>
+        );
+    });
+
+    return (
+        <div>
+            <h1 className="txt-shadow-blue">People</h1>
+            <button
+                onClick={previousPage}
+                disabled={starWarsDataPeople.previous ? false : true}
+            >
+                ⏪ Previous Page
+            </button>
+            <button
+                onClick={nextPeoplePage}
+                disabled={starWarsDataPeople.next ? false : true}
+            >
+                Next Page⏩
+            </button>
+
+            <main>{allPeopleOnPage}</main>
+        </div>
+    );
+
+    function nextPeoplePage() {
+        setLoading(true);
+        setUrlPeople(starWarsDataPeople.next);
+    }
+
+    function previousPage() {
+        setLoading(true);
+        setUrlPeople(starWarsDataPeople.previous);
+    }
+}
+
+export default People;
